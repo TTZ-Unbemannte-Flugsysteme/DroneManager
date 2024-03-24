7377ae11b2406b2d662f14e802a3522ff4d7d512
Domattee <domattee@yahoo.de>
2024-03-24 21:20:07 +0100

    Input now has a history

--- dronecontrol.py
+++ dronecontrol.py
@@ -11,6 +11,7 @@ from textual import on
 from textual.app import App
 from textual.containers import Horizontal, Vertical
 from textual.widgets import Footer, Header, Input, Log, Static
+from textual.binding import Binding
 
 from drones import Drone, DroneMAVSDK, DummyMAVDrone, parse_address
 
@@ -52,7 +53,6 @@ class DroneManager(App):
     # TODO: Test with actual drones.
     # TODO: Handle mavsdk errors (such as offboard error) better. Currently prints either a DENIED message with little
     #   information or an even more useless "<ErrorObject at 0x...>".
-
 
     # How often the status screen is updated.
     STATUS_REFRESH_RATE = 5
@@ -307,7 +307,7 @@ class DroneManager(App):
         output = self.query_one("#output", expect_type=Log)
         output.write_line(f"Sending {name} to {point}")
         try:
-            result = await self.drones[name].fly_to_point(point, tolerance=tol)
+            await self.drones[name].fly_to_point(point, tolerance=tol)
             output.write_line(f"{name} arrived at {point}!")
         except KeyError:
             output.write_line(f"No drone named {name}!")
@@ -411,7 +411,7 @@ class DroneManager(App):
                     id="sidebar",
                 )
             ),
-            Input(placeholder="Command line", id="cli")
+            InputWithHistory(placeholder="Command line", id="cli")
         )
 
         yield Footer()
@@ -430,6 +430,63 @@ class DroneManager(App):
             return inner
         sys.stdout.write = decorator(sys.stdout.write)
         sys.stderr.write = decorator(sys.stderr.write)
+
+
+class InputWithHistory(Input):
+
+    BINDINGS = Input.BINDINGS.copy()
+    BINDINGS.append(Binding("up", "history_prev", "Previous item from history", show=False))
+    BINDINGS.append(Binding("down", "history_rec", "Next item in history", show=False))
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.history = [""]
+        self.history_cursor = 0     # Shows where in the history we are
+        self.rolling_history_pos = 0
+        self.max_length = 50
+
+    # Behaviour: Depends on if we are already using history.
+    # A new submission is added to the history
+    # Up then goes back into the history
+    # Down does nothing
+    # But: if we submit a history entry (i.e. go back and do not change)
+    # Up displays the same entry (from the history)
+    # Down shows the next entry in the history.
+    # i.e. submit "a", up -> "a", down -> nothing
+    # submit "a", submit "b", submit "c", up -> "c", up ->"b", submit "b", [up -> "b", down ->"c"]
+
+    @property
+    def _current_history_cursor(self):
+        return (self.rolling_history_pos - self.history_cursor) % min(len(self.history), self.max_length)
+
+    def _increase_history_cursor(self):
+        if self.history_cursor < self.max_length - 1 and self.history_cursor < len(self.history) - 1:
+            self.history_cursor += 1
+
+    def _decrease_history_cursor(self):
+        if self.history_cursor > 0:
+            self.history_cursor -= 1
+
+    def action_history_prev(self) -> None:
+        self._increase_history_cursor()
+        self.value = self.history[self._current_history_cursor]
+
+    def action_history_rec(self) -> None:
+        self._decrease_history_cursor()
+        self.value = self.history[self._current_history_cursor]
+
+    def add_to_history(self, item) -> None:
+        # If we add extra entries, overwrite old ones
+        if len(self.history) == self.max_length:
+            # Entry to be overriden is at rolling_history_pos
+            self.history[self.rolling_history_pos] = item
+        else:
+            self. history.append(item)
+
+    async def action_submit(self) -> None:
+        self.add_to_history(self.value)
+        self.history_cursor = 0     # TODO: Fancy history maintaining
+        await super().action_submit()
 
 
 if __name__ == "__main__":
